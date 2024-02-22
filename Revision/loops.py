c7875cb35b726a1471a669d892b9c41013b4997b
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-02-23 03:24:59 +0530

    Basics Revision done

---
+++ Revision/loops.py
@@ -0,0 +1,10 @@
+for i in [1,2,3,4,5,6,7]:
+    print(i)
+    
+for j in range(30):
+    print(j)
+    
+name = "Dinesh "
+
+for i in name: 
+    print(i)
---
+++ Revision/variable.py
@@ -0,0 +1,4 @@
+a = 28 
+b = "Dinesh"
+c = True
+d = None #none type of varibale 
